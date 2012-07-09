ca3d979a0c077c1b261ccf572298242f9f65a6c0
Daniel Lee <lee@isi-solutions.org>
2012-07-09 20:10:30 +0200

    Added bare bones python scaffolding.

---
+++ peak_parameters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+##############################################################################
+#
+# SCRIPT:       peak_parameters.py
+# AUTHOR(S):    Nico Caspari & Daniel Lee
+# PURPOSE:      Identifies peaks using the methodology in r.param.scale and
+#               compares them to a list of peaks entered by the user.
+#               Advanced GIS (Dr. C. Reudenbach).
+#
+##############################################################################
+
+def main():
+    # Find peaks using different windows
+    # 3x3, 5x5, 9x9, 19x19, 39x39, 69x69
+        # For each window, iterate with slope threshold 1-10
+    
+    # Extract peaks and convert them to vectors
+    
+    # Produce matrices for each method normalized for the number of peaks
+    # Contains query for vectors and peak points
+        # True positives
+        # False positives
+        # False negatives
+    return
+
+if __name__ == '__main__':
+    main()
