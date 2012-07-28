8bac39724061a773da1691317b9ec497b02b5423
Caspari <caspari@students.uni-marburg.de>
2012-07-28 13:27:45 +0200

    code for exportToCsv function added. Still needs extension for summary.

--- peak_parameters.py
+++ peak_parameters.py
@@ -39,29 +39,56 @@ class Exporter(object):
     '''
     Summarizes results and exports them to a specified format.
     '''
+
+    import csv
     
     def summarize(self):
-        '''summarizes ReslutContainer error values to error index.'''
+        '''summarizes ResultContainer error values to error index.'''
         return
     
-    def exportToCsv(self, id):
+    def exportToCsv(self, container, id, exportpath):
         ''' 
-        Depending on id, exports error matrix from ResultContainer to csv file.
-        
-        Args: 
+        Depending on id, exports error values from a ResultContainer to csv.
+        Args:
+            container: ResultContainer object which shall be exported
             id: error value or summary to be exported
-                1 = 'false positive',
-                2 = 'true negative',
-                3 = 'false negative'
-                4 = summarized index
+                0 = 'false positive',
+                1 = 'true negative',
+                2 = 'false negative'
+                3 = summarized index
+            exportpath: path where file shall be created, plus FILENAME.csv
         '''
+        self.container = container
+        self.id = id
+        self.exportpath = exportpath
+        
+        # could test the integrity of cointainer file: 
+        # isinstance(container, ResultContainer)  
+        
+        file = open(self.exportpath, "wb")
+        csvWriter = csv.writer(file)
+
+        # run through each window of ResultContainer
+        for window in range(0, len(container.matrix)):
+            errList = []  # set up list of error values for current window
+            #  run through each threshold
+            for threshold in range(0, len(container.matrix[window])):
+                # append error values to error list
+                errList.append(container.matrix[window][threshold][self.id])
+                
+            csvWriter.writerow(errList) # write error vals for window to file
+        
+        file.close()  # close file path        
         return
+    
     
     def stdout(self):
         '''somehow create standard out for GRASS console''' 
+        
+        # z.B. feste Leerzeichenzahl -> 8-len(str(Zahl)) Leerzeichen
+        
         return 
     
-    return
 
 def main():
     window_sizes = [3, 5, 9, 19, 39, 69]
