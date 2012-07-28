b3712aceca56f962213226c60254bf07c0e8042d
Caspari <caspari@students.uni-marburg.de>
2012-07-28 18:45:35 +0200

    exportToCsv() updated. Should be done now.

--- peak_parameters.py
+++ peak_parameters.py
@@ -62,20 +62,21 @@ class Exporter(object):
         sensitivity = tp/(tp+fn) 
         return sensitivity
     
-    def exportToCsv(self, container, id, exportpath):
+    def exportToCsv(self, container, errTag, exportpath):
         ''' 
         Depending on id, exports error values from a ResultContainer to csv.
         Args:
             container: ResultContainer object which shall be exported
-            id: error value or summary to be exported
-                0 = 'true positives',
-                1 = 'false positives',
-                2 = 'false negatives'
-                3 = summarized index
+            errTag: Tag to indicate, which error or summary shall be exported.
+                accepted strings:
+                    'true positives',
+                    'false positives',
+                    'false negatives'
+                    'summarize'
             exportpath: path where file shall be created, plus FILENAME.csv
         '''
         self.container = container
-        self.id = id
+        self.errTag = errTag
         self.exportpath = exportpath
  
         # get indexes of error values in ResultContainer 
@@ -83,27 +84,30 @@ class Exporter(object):
         fp_index = container.error_values.index('false positives')
         fn_index = container.error_values.index('false negatives') 
         
-        # could test the integrity of container file: 
-        # isinstance(container, ResultContainer)  
-        
         file = open(self.exportpath, 'wb')
         csvWriter = csv.writer(file)
 
         # run through each window of ResultContainer
-        for window in range(len(container.matrix)):
+        for window in range(len(container.window)):
             errList = []  # set up list of error values for current window
             #  run through each threshold
-            for threshold in range(len(container.matrix[window])):
+            for threshold in range(len(container.window[window])):
                 # if summary mode was selected, call summarize()
-                if (self.id == 3): 
-                    tp = container.matrix[window][threshold][tp_index]
-                    fp = container.matrix[window][threshold][fp_index]
-                    fn = container.matrix[window][threshold][fn_index]
+                if (self.errTag == 'summarize'): 
+                    tp = container.window[window][threshold][tp_index]
+                    fp = container.window[window][threshold][fp_index]
+                    fn = container.window[window][threshold][fn_index]
                     summary = self.summarize(tp, fp, fn)
                     errList.append(summary)
+                
                 # else append error value to error list
-                else:
-                    errList.append(container.matrix[window][threshold][self.id])
+                elif (self.errTag == 'true positives'): 
+                    errList.append(container.window[window][threshold][tp_index])
+                elif (self.errTag == 'false positives'): 
+                    errList.append(container.window[window][threshold][fp_index])
+                elif (self.errTag == 'false negatives'): 
+                    errList.append(container.window[window][threshold][fn_index])
+                else: raise(Exception)                
                 
             csvWriter.writerow(errList) # write error vals for window to file
         
@@ -113,8 +117,12 @@ class Exporter(object):
     
     def stdout(self):
         '''somehow create standard out for GRASS console''' 
-        
+        # feste Breite auf 80 Zeichen
+        # oben in die Ecke: window/threshold
         # z.B. feste Leerzeichenzahl -> 8-len(str(Zahl)) Leerzeichen
+        # Oder: Methode r.just, z.B. : entry += value.rjust(7)
+
+        
         
         return 
     
